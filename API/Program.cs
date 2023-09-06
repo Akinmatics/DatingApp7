be1b9fea3858ca9a118015b5b29e08c40cfa17ff
Akinmatics <ridwanakinola1@gmail.com>
2023-09-06 21:03:56 +0100

    S2- api skeleton

---
+++ API/Program.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Immutable;
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext<DataContext>(opt =>
+{
+    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+var app = builder.Build();
+
+//Configure the http request pipeline
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
